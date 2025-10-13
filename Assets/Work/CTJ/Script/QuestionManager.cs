588ca90f2623e8b5d1da7f3bccdbbef146f6a1bd
ARKANOS_ <Kolonaiden@gmail.com>
2025-10-13 15:32:07 +0900

    QuestionManager

--- Assets/Work/CTJ/Script/QuestionManager.cs
+++ Assets/Work/CTJ/Script/QuestionManager.cs
@@ -1,9 +1,13 @@
+using System.Collections;
 using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
 public class QuestionManager : MonoBehaviour
 {
+    public int _currentQuestionNum = 1;
+    [SerializeField] private string[] _questions;
+
     [SerializeField] private GameObject questionPanel;
     [SerializeField] private GameObject OPanel;
     [SerializeField] private GameObject XPanel;
@@ -11,5 +15,44 @@ public class QuestionManager : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _questionText;
     [SerializeField] private TextMeshProUGUI _numText;
 
-    Sequence sequence = DOTween.Sequence();
+    [SerializeField] private float _timeDelayed = 5.0f;
+
+
+    private void Start()
+    {
+        StartQuestions();
+    }
+
+    public void OtextOn()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(OPanel.transform.DOMoveY(0, 0.5f));
+        StartCoroutine(TimeDelayed(OPanel));
+    }
+
+    public void XtextOn()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(XPanel.transform.DOMoveY(0, 0.5f));
+        StartCoroutine(TimeDelayed(XPanel));
+    }
+
+    IEnumerator TimeDelayed(GameObject panel)
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        yield return new WaitForSeconds(_timeDelayed);
+
+        sequence.Append(panel.transform.DOMoveY(-150, 0.5f));
+        sequence.Kill();
+    }
+
+    public void StartQuestions()
+    {
+        _currentQuestionNum++;
+        _numText.text = $"{_currentQuestionNum}.";
+        _questionText.text = _questions[_currentQuestionNum - 1];
+    }
 }
